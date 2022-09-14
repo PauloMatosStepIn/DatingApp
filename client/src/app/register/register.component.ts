ee2ee9ff3e3cdcfbf47b0b9deb17d100eacf1f9c
Paulo Matos <paulomatos@stepinportugal.com>
2022-09-14 23:00:42 +0100

    extending form fields, form builder

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -16,19 +16,35 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
   constructor(private accountService: AccountService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
+
+
   initializeForm() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+
+    // Reactive Forms
+    //   this.registerForm = new FormGroup({
+    //     username: new FormControl('', Validators.required),
+    //     password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+    //     confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    //   })
+
+    // Reactive Forms w/Form Builder 
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     })
-    this.registerForm.controls.password.valueChanges.subscribe(()=>{
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
       this.registerForm.controls.confirmPassword.updateValueAndValidity();
     })
   }
